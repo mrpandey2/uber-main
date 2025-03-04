3e96d7c56408f05e5a3c55b0235ddce369baf970
prashu12 <pandeyprashantganesh@gmail.com>
2025-03-04 07:34:56 +0530

    Added Dto's

---
+++ src/main/java/com/project/uber/dto/PointDto.java
@@ -0,0 +1,16 @@
+package com.project.uber.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SecondaryRow;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PointDto {
+
+    private double[] coordinates;
+    private String type = "Points";
+
+}
---
+++ src/main/java/com/project/uber/dto/RideStartDto.java
@@ -0,0 +1,14 @@
+package com.project.uber.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RideStartDto {
+
+    private String otp;
+
+}
