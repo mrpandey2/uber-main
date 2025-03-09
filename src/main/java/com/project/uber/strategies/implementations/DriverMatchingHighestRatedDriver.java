6e1429e8fffefdb81e6738b5cbb7585b7a6b103e
prashu12 <pandeyprashantganesh@gmail.com>
2025-03-10 03:46:23 +0530

    Added Strategies for Ride booking,Payment and Fare calculation

---
+++ src/main/java/com/project/uber/strategies/implementations/DriverMatchingHighestRatedDriver.java
@@ -0,0 +1,23 @@
+package com.project.uber.strategies.implementations;
+
+import com.project.uber.entities.Driver;
+import com.project.uber.entities.RideRequest;
+import com.project.uber.repositories.DriverRepository;
+import com.project.uber.strategies.DriverMatchingStrategy;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class DriverMatchingHighestRatedDriver implements DriverMatchingStrategy {
+
+    private final DriverRepository driverRepository;
+
+
+    @Override
+    public List<Driver> findMatchingDrivers(RideRequest rideRequest) {
+        return driverRepository.findTenNearbyTopRatedDrivers(rideRequest.getPickUpLocation());
+    }
+}
